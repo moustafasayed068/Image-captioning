d0f09d554d04cefb5df70d0d0d9a39dcc02e1a0e
moustafasayed068 <moustafasayed068@gmail.com>
2026-04-08 20:31:23 +0000

    Revert "refac:project structure"

--- app/dependencies.py
+++
@@ -1,5 +0,0 @@
-from app.services.caption_service import CaptionService
-
-
-def get_caption_service() -> CaptionService:
-    return CaptionService()
--- app/models/image_caption.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from datetime import datetime
-
-
-@dataclass
-class ImageCaption:
-    image_url: str
-    caption: str
-    created_at: datetime = datetime.utcnow()
